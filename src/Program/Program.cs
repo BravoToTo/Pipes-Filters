789a9bfd296b4bd6806ad1e4fb590c09fd8a7b29
BravoToTo <francosanmartin@adinet.com.uy>
2022-10-17 21:01:17 -0300

    Changes

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,17 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"beer.jpg");
+
+            PipeNull ReturnPipe = new PipeNull();
+
+            PipeSerial pipe2 = new PipeSerial(new FilterNegative(), ReturnPipe);
+            // PipeFork Fork1 = new PipeFork(pipe2, ReturnPipe);
+            PipeSerial pipe1 = new PipeSerial(new FilterGreyscale(), pipe2);
+
+            picture = pipe1.Send(picture);
+            provider.SavePicture(picture, @"Beer_Filtered.jpg");
         }
     }
 }
