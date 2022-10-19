0fbfd7bfcfbd25c7159a36a32f64027c5f839f61
BravoToTo <francosanmartin@adinet.com.uy>
2022-10-19 19:59:48 -0300

    Update

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using CompAndDel.Pipes;
 using CompAndDel.Filters;
+using TwitterUCU;
 
 namespace CompAndDel
 {
@@ -9,7 +10,7 @@ namespace CompAndDel
         static void Main(string[] args)
         {
             PictureProvider provider = new PictureProvider();
-            IPicture picture = provider.GetPicture(@"beer.jpg");
+            IPicture picture = provider.GetPicture(@"luke.jpg");
             FilterStoreImage SaveFilter = new FilterStoreImage();
 
             PipeNull ReturnPipe = new PipeNull();
@@ -18,7 +19,12 @@ namespace CompAndDel
             PipeSerial pipe1 = new PipeSerial(new FilterGreyscale(), SavePipe1);
 
             picture = pipe1.Send(picture);
-            provider.SavePicture(picture, "Beer_Filtered.jpg");
+            provider.SavePicture(picture, "Final.jpg");
+
+            // var twitter = new TwitterImage();
+            // Console.WriteLine(twitter.PublishToTwitter("Beer", @"Beer_Filtered.jpg"));
+            // var twitterDirectMessage = new TwitterMessage();
+            // Console.WriteLine(twitterDirectMessage.SendMessage("Hola!", "1396065818"));
         }
     }
 }
