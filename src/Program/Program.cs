93fb00001c6cfc7dcf2f208caf55aafab0a5ca34
BravoToTo <francosanmartin@adinet.com.uy>
2022-10-23 22:39:03 -0300

    Comments

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -11,23 +11,33 @@ namespace CompAndDel
         {
             PictureProvider provider = new PictureProvider();
             IPicture picture = provider.GetPicture(@"luke.jpg");
-            // FilterStoreImage SaveFilter = new FilterStoreImage();
 
+            // --- Return Pipe needed for all sequences ---
             PipeNull ReturnPipe = new PipeNull();
+            // --------------------------------------------
+
+            // --- Save Steps Sequence ---
             // PipeSerial pipe2 = new PipeSerial(new FilterNegative(), ReturnPipe);
             // PipeSerial SavePipe1 = new PipeSerial(SaveFilter, pipe2);
             // PipeSerial pipe1 = new PipeSerial(new FilterGreyscale(), SavePipe1);
-            PipeSerial pipe3 = new PipeSerial(new FilterNegative(), ReturnPipe);
-            PipeSerial pipe2 = new PipeSerial(new FilterGreyscale(), ReturnPipe);
-            PipeFork ConditionalPipe = new PipeFork(pipe2, pipe3);
+            // picture = pipe1.Send(picture);
+            // ---------------------------
 
+            // --- Conditional Pipe Sequence ---
+            PipeSerial PipeNegative = new PipeSerial(new FilterNegative(), ReturnPipe);
+            PipeSerial PipeGrey = new PipeSerial(new FilterGreyscale(), ReturnPipe);
+            PipeFork ConditionalPipe = new PipeFork(PipeGrey, PipeNegative);
             picture = ConditionalPipe.Send(picture);
-            provider.SavePicture(picture, "Final.jpg");
+            // ---------------------------------
 
+            // --- Save Picture To Local ---
+            provider.SavePicture(picture, "Final.jpg");
+            // -----------------------------
+
+            // --- Twitter API (Publish) ---
             // var twitter = new TwitterImage();
-            // Console.WriteLine(twitter.PublishToTwitter("Beer", @"Beer_Filtered.jpg"));
-            // var twitterDirectMessage = new TwitterMessage();
-            // Console.WriteLine(twitterDirectMessage.SendMessage("Hola!", "1396065818"));
+            // Console.WriteLine(twitter.PublishToTwitter("Picture", @"FILENAME_HERE.jpg"));
+            // -----------------------------
         }
     }
 }
